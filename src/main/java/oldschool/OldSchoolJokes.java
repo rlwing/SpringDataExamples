9058f2bb27bd86fc540ecd4f7b82f1e3f91d6b2b
rlwing <rob@wingjr.com>
2019-06-20 11:49:13 -0700

    added OldSchoolJokes

--- src/main/java/oldschool/OldSchoolJokes.java
+++ src/main/java/oldschool/OldSchoolJokes.java
@@ -1,4 +1,29 @@
 package oldschool;
 
-public class OldSchoolJoke {
+import java.sql.*;
+
+public class OldSchoolJokes {
+    public String getRandomJoke() {
+        String joke = null;
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jokes", "root", null);
+            ps = conn.prepareStatement("SELECT * from jokes j order by RAND() LIMIT 1");
+            rs = ps.executeQuery();
+            if(rs.next()) joke = rs.getString("joke");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (conn != null) try{ conn.close(); }catch (Exception ignore){}
+            if (ps != null) try{ ps.close(); }catch (Exception ignore){}
+            if (rs != null) try{ rs.close(); }catch (Exception ignore){}
+        }
+        return joke;
+    }
 }
--- src/test/java/oldschool/OldSchoolJokesTests.java
+++ src/test/java/oldschool/OldSchoolJokesTests.java
@@ -1,13 +1,22 @@
 package oldschool;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class RandomJokeOldScoolTest {
+public class OldSchoolJokesTests {
 
     @Test
     public void getRandomJokeOldSchool() {
-        String joke;
 
-        OldSchoolJoke osj = new OldSchoolJoke();
+        OldSchoolJokes osj = new OldSchoolJokes();
+
+        String joke = osj.getRandomJoke();
+        assertNotNull(joke);
+        System.out.println("joke 1 = "+joke+"\n");
+
+        String joke2 = osj.getRandomJoke();
+        assertNotNull(joke2);
+        assertNotEquals(joke2, joke);
+        System.out.println("joke 2 = "+joke2);
     }
 }
